390cee3173999ea4f739649f4d5f2f92d44814b7
Matheus Battisti <matheusbudkewicz@gmail.com>
2023-05-16 08:29:13 -0300

    first commit

---
+++ src/components/Todo.jsx
@@ -0,0 +1,23 @@
+const Todo = ({ todo, completeTodo, removeTodo }) => {
+  return (
+    <div
+      className="todo"
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+    >
+      <div className="content">
+        <p>{todo.text}</p>
+        <p className="category">({todo.category})</p>
+      </div>
+      <div>
+        <button className="complete" onClick={() => completeTodo(todo.id)}>
+          Completar
+        </button>
+        <button className="remove" onClick={() => removeTodo(todo.id)}>
+          x
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
